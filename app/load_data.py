0531ee2051c295f04327f1c858dfec1a46efc67b
Maggie Stewart <magstewart@gmail.com>
2018-01-22 14:22:23 -0800

    update load_data.py to read from database

--- app/load_data.py
+++ app/load_data.py
@@ -1,8 +1,20 @@
 import pandas as pd
+import psycopg2
 
 class DataModel():
     def __init__(self):
-        self.data = pd.read_csv("~/galvanize/wildfire-project/data/predictions.csv")
+        conn = psycopg2.connect("host=firesdbinstance.cwspjcvdc38q.us-west-2.rds.amazonaws.com port=5432 user=Administratos password=IGB9837nkdywf dbname=fires")
+        cur = conn.cursor()
+
+        cur.execute('''SELECT StartDate, Latitude, Longitude, Probability
+                       FROM current_fires ORDER BY Probability DESC
+                       LIMIT 30''')
+
+        self.data = cur.fetchall()
+
+        cur.close()
+        conn.close()
+
 
     def get_top_fires(self):
-        return self.data.head(5).to_dict("records")
+        return self.data
