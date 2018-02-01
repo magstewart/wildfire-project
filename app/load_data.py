e0a2063e259f70ab04b12eb14620aa57395a8e70
Maggie Stewart <magstewart@gmail.com>
2018-02-01 10:56:08 -0800

    fix file typo

--- app/load_data.py
+++ app/load_data.py
@@ -27,8 +27,8 @@ class DataModel():
     def predict_single(self, d):
         s3 = boto3.resource('s3')
         BUCKET_NAME = 'wildfire-project-data'
-        s3.meta.client.download_file(BUCKET_NAME, 'data/clean_weather_data.csv', '/data/clean_weather_data_test.csv')
-        s3.meta.client.download_file(BUCKET_NAME, 'data/final_grid_probs.csv','/data/final_grid_probs.csv')
+        s3.meta.client.download_file(BUCKET_NAME, 'data/clean_weather_data.csv', 'data/clean_weather_data.csv')
+        s3.meta.client.download_file(BUCKET_NAME, 'data/final_grid_probs.csv','data/final_grid_probs.csv')
 
         one_df = pd.DataFrame(list(d.values())).T
         one_df.columns = list(d.keys())
